3d08f9dd3c8f82aebd2601043058c24ae7a0d8bf
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2024-08-01 22:04:42 +0000

    test preview

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ export default class Footer extends React.Component<FooterContent> {
       <div className="bg-gray-900 text-white p-10">
         <div className="flex flex-col md:flex-row items-start justify-center md:justify-between space-y-6 md:space-y-0 md:space-x-10 md:w-30">
           <div className="w-full md:w-1/2 lg:w-1/3 flex-grow text-center md:text-center">
-            <h2 className="font-bold mb-4">CONTACTEZ-NOUS</h2>
+            <h2 className="font-bold mb-4">CONTACTEZ-NOUS PREVIEW</h2>
             <span className="block" >{adresse}</span>
             <span className="mt-2 block">Téléphone : {tel}</span>
             <span>Email : <a href={`mailto:${email}`} className="underline">{email}</a></span>
