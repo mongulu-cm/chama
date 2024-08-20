1d8d5151676cdf35460678b8848f9c04e1af784f
Tchepga <azera9730@gmail.com>
2024-08-20 23:04:11 +0200

    fix menu style + mot du president

--- frontend/src/pages/welcome/Welcome.tsx
+++ frontend/src/pages/welcome/Welcome.tsx
@@ -46,34 +46,36 @@ class Welcome extends React.Component<Content, unknown> {
 
     const urlDirectusAssets = ContentService.api_url_assets;
     const urlPhotoPresident = this.props?.associationInfo?.photo_president;
-    const imagePresident = urlPhotoPresident 
-      ? <img src={`${urlDirectusAssets}/${urlPhotoPresident}`} alt='photo du president' className='w-1/3 rounded-full' /> 
+    const imagePresident = urlPhotoPresident
+      ? <img src={`${urlDirectusAssets}/${urlPhotoPresident}`} alt='Le president' className='w-1/3 rounded-tl-full rounded-bl-full rounded-br-full p-5' />
       : <IconButton><PersonIcon /></IconButton>;
     return (
       <div className='flex flex-col gap-10'>
         {subMenuTag}
         <div className='flex flex-col items-center w-2/3 mx-auto min-w-[90%] md:min-w-min'>
           <h2 className='text-4xl mb-2'>Qui sommes-nous ?</h2>
-          <p className='text-center '>{this.props?.description}</p>
+          <div className='text-center '>{this.props?.description}</div>
           <Link to='/contactez-nous'>
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>contactez-nous</button>
           </Link>
         </div>
         <div className='flex flex-col items-center w-2/3 mx-auto'>
           <h2 className='text-4xl mb-8'>Nos derniers projects</h2>
-          <p className='text-center flex gap-6 flex-col md:flex-row min-w-[90%] md:min-w-min'>
-          {projectstag}
-          </p>
+          <div className='text-center flex gap-6 flex-col md:flex-row min-w-[90%] md:min-w-min'>
+            {projectstag}
+          </div>
         </div>
 
         {/* mot du president */}
         <div className='flex flex-col items-center w-2/3 mx-auto mb-4 min-w-[90%] md:min-w-min'>
-          <h2 className='text-4xl mb-8'>Mot du président</h2>
-          <div className='flex gap-6'>
-           {imagePresident}
-            <div className='flex flex-col gap-4'>
-              <h3 className='text-2xl font-bold'>{this.props?.associationInfo?.nom_du_president}</h3>
+          <div className='flex gap-6 flex-col md:flex-row items-center'>
+            {imagePresident}
+            <div className=' w-2/3 flex flex-col gap-4'>
               <p>{this.props?.associationInfo?.mot_du_president}</p>
+              <div className='flex flex-col'>
+                <h4 className='text-2xl font-bold flex-grow text-blue-600'>{this.props?.associationInfo?.nom_du_president}</h4>
+                <span>Le Président</span>
+              </div>
             </div>
           </div>
         </div>
