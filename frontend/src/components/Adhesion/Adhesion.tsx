a399c27cef44dc15c73820a8eb0e8a4640a2edd0
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2024-09-20 20:53:09 +0000

    add adhesion form

--- frontend/src/components/Adhesion/Adhesion.tsx
+++ frontend/src/components/Adhesion/Adhesion.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 export default class Adhesion extends React.Component {
     render() {
+        const iframeStyle = {
+            width: '100%',
+            height: '750px',
+            border: 'none'
+        };
 
         return (
-            <div>Pas de média</div>
-       )
+            <div>
+                <iframe 
+                    id="haWidget" 
+                    allowTransparency={true} 
+                    scrolling="auto" 
+                    src="https://www.helloasso.com/associations/collectif-mongulu/adhesions/formulaire-ahesion/widget" 
+                    style={iframeStyle}
+                ></iframe>
+            </div>
+        );
     }
 }
